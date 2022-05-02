32d880ed5cc3e5f85d4aed12d5870d755a718fc7
PrafullaShete <prafulla.shete@arieotech.com>
2022-05-02 12:51:00 +0530

    Changes for UserMaster Web Api

---
+++ EcommerceAPI/EcommerceAPI/Controllers/UserMasterController.cs
@@ -0,0 +1,64 @@
+﻿using EcommerceAPI.Model;
+using EcommerceAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserMasterController : Controller
+    {
+        #region Configuration
+        protected readonly IConfiguration _config;
+        private readonly IUserMaster user;
+
+        public UserMasterController(IUserMaster _user)
+        {
+            this.user = _user;
+        }
+        #endregion
+
+        #region Insert New User
+        [HttpPost("InsertNewUser")]
+        public IActionResult InsertNewUser(UserMaster add)
+        {
+            IActionResult result;
+            try
+            {
+                string Newuserdata=this.user.InsertNewUser(add);
+                result = Ok(Newuserdata);
+            }
+            catch (Exception ex)
+            {
+                result = new StatusCodeResult(500);
+                return BadRequest(string.Format(ex.Message));
+            }
+            return result;
+        }
+        #endregion
+
+        #region GetUserByUserId
+        [HttpGet("GetUserByUserId")]
+        public IActionResult GetUserByUserId(int Id)
+        {
+            IActionResult result;
+            try
+            {
+                IEnumerable<UserMaster> obj = this.user.GetUserByUserId(Id);
+                result = Ok(obj);
+            }
+            catch (Exception ex)
+            {
+                result = new StatusCodeResult(500);
+                return BadRequest(string.Format(ex.Message));
+            }
+            return result;
+        }
+        #endregion
+    }
+}
---
+++ EcommerceAPI/EcommerceAPI/Model/UserMaster.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceAPI.Model
+{
+    public class UserMaster
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public int RoleId { get; set; }
+        public DateTime DateOfRegistration { get; set; }
+        public DateTime DateOfActivation { get; set; }
+        public DateTime DateOfPlanExpiry { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public int Active { get; set; }
+        public string OTP { get; set; }
+        public DateTime OTPTime { get; set; }
+    }
+}
