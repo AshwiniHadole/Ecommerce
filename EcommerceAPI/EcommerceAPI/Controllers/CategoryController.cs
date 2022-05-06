a680ca5aeb1fdd55f4a92fd3f8947e2d7be29735
Prafulla Shete <prafulla.shete@arieotech.com>
2022-05-06 18:14:49 +0530

    Changes for ModelValidation,Nunit assertions for usermaster and category API and Response model addition.

--- EcommerceAPI/EcommerceAPI/Controllers/CategoryController.cs
+++ EcommerceAPI/EcommerceAPI/Controllers/CategoryController.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace EcommerceAPI.Controllers
@@ -24,16 +25,20 @@ namespace EcommerceAPI.Controllers
         public IActionResult GetCategoryById(int Id)
         {
             IActionResult result;
+            ResponseModel res = new ResponseModel();
             try
             {
                 this.logger.LogInfo("Get All Details by Id.");
-                IEnumerable<Category> ById = this.category.GetCategoryById(Id);
+                IEnumerable<Category> category = this.category.GetCategoryById(Id);
+                res.Data = category;
+                res.Message = "GetCategoryById executed successfully.";
+                res.statusCode = HttpStatusCode.OK.ToString();
                 this.logger.LogInfo("All Data Displayed.");
-                result = Ok(ById);
+                result = Ok(res);
             }
             catch (Exception ex)
             {
-                result = new StatusCodeResult(500);
+                res.statusCode = HttpStatusCode.BadRequest.ToString();
                 this.logger.LogInfo(string.Format(ex.Message));
                 return BadRequest("Internal Server Error");
             }
@@ -44,16 +49,20 @@ namespace EcommerceAPI.Controllers
         public IActionResult GetCategoryByStoreId(int StoreId)
         {
             IActionResult result;
+            ResponseModel res = new ResponseModel();
             try
             {
                 this.logger.LogInfo("Get All Details By StoreId.");
-                IEnumerable<Category> ByStoreId = this.category.GetCategoryByStoreId(StoreId);
+                IEnumerable<Category> _category = this.category.GetCategoryByStoreId(StoreId);
+                res.Data = _category;
+                res.Message = "GetCategoryByStoreId executed successfully.";
+                res.statusCode = HttpStatusCode.OK.ToString();
                 this.logger.LogInfo("All Data Displayed.");
-                result = Ok(ByStoreId);
+                result = Ok(res);
             }
             catch (Exception ex)
             {
-                result = new StatusCodeResult(500);
+                res.statusCode = HttpStatusCode.OK.ToString();
                 this.logger.LogInfo(string.Format(ex.Message));
                 return BadRequest("Internal Server Error.");
             };
@@ -64,16 +73,19 @@ namespace EcommerceAPI.Controllers
         public IActionResult DeleteCategoryById(int Id)
         {
             IActionResult result;
+            ResponseModel res = new ResponseModel();
             try
             {
                 this.logger.LogInfo("Delete Category By Id.");
                 this.category.DeleteCategoryById(Id);
+                res.Message = "DeleteCategoryById executed successfully.";
+                res.statusCode = HttpStatusCode.OK.ToString();
                 this.logger.LogInfo("Category Deleted Successfully.");
                 result = Ok("Category Deleted Successfully.");
             }
             catch (Exception ex)
             {
-                result = new StatusCodeResult(500);
+                res.statusCode = HttpStatusCode.OK.ToString();
                 this.logger.LogInfo(string.Format(ex.Message));
                 return BadRequest("Internal Server Error.");
             };
@@ -84,16 +96,20 @@ namespace EcommerceAPI.Controllers
         public IActionResult UpdateCategoryById(Category set)
         {
             IActionResult result;
+            ResponseModel res = new ResponseModel();
             try
             {
                 this.logger.LogInfo("Update Category By Id.");
                 string A = this.category.UpdateCategoryById(set);
+                res.Data = A;
+                res.Message = "UpdateCategoryById executed successfully.";
+                res.statusCode = HttpStatusCode.OK.ToString();
                 this.logger.LogInfo("Category Updated Successfully.");
-                result = Ok(A);
+                result = Ok(res);
             }
             catch (Exception ex)
             {
-                result = new StatusCodeResult(500);
+                res.statusCode = HttpStatusCode.OK.ToString();
                 this.logger.LogInfo(string.Format(ex.Message));
                 return BadRequest("Internal Server Error.");
             };
@@ -104,16 +120,20 @@ namespace EcommerceAPI.Controllers
         public IActionResult InsertNewCategory(Category add)
         {
             IActionResult result;
+            ResponseModel res = new ResponseModel();
             try
             {
                 this.logger.LogInfo("Insert Category.");
                 string newcategory = this.category.InsertNewCategory(add);
+                res.Data = newcategory;
+                res.Message = "InsertNewCategory executed successfully.";
+                res.statusCode = HttpStatusCode.OK.ToString();
                 this.logger.LogInfo("Category Inserted Successfully.");
-                result = Ok(newcategory);
+                result = Ok(res);
             }
             catch (Exception ex)
             {
-                result = new StatusCodeResult(500);
+                res.statusCode = HttpStatusCode.OK.ToString();
                 this.logger.LogInfo(string.Format(ex.Message));
                 return BadRequest("Internal Server Error.");
             };
